9c0ca055c8ce7249dff318df3c35d1d69b1108c0
Kyle <kyl3gonzalez22@gmail.com>
2019-02-24 20:05:18 -0700

    4th and final commit

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,4 +1,5 @@
 package cse360assign2;
+
 /*
  * Represents a calculator that is able to perform add, subtract, multiply, and divide operations.
  * 
@@ -21,7 +22,7 @@ public class Calculator {
 	 * Constructor that allows usage of the methods in this class that will be used to manipulate the total variable.
 	 */
 	public Calculator () {
-		total = 0;  // not needed - included for clarity
+		
 	}
 	
 	/*
